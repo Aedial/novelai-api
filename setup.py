16ac4dfbe9be2aee2ba2d53f62ab1df929952cd8
Aedial <aedial.dev@gmail.com>
2021-12-21 01:53:01 +0100

    [API] Add story manager (WIP)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "novelai-api",
-    version = "0.5",
+    version = "0.6",
     author = "Arthus Leroy",
     author_email = "arthus.leroy@epita.fr",
     url = "https://github.com/arthus-leroy/novelai-api/",
