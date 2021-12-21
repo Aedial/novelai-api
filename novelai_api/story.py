55b0759d24226e415222b9b90ae5de1cc4084f95
Arthus Leroy <arthus.leroy@gmail.com>
2021-12-21 18:44:13 +0100

    [API] Clean story manager a bit and begin implementation

--- novelai_api/story.py
+++ novelai_api/story.py
@@ -1,9 +1,13 @@
 from novelai_api import NovelAI_API
+from novelai_api.utils import encrypt_user_data
+
 from nacl.secret import SecretBox
+from nacl.utils import random
 
 from typing import Dict, List, NoReturn, Any, Optional, Union
-from os import urandom
 from uuid import uuid4
+from time import time
+
 
 DEFAULT_MODEL = "6B-v3"
 DEFAULT_PARAMS = {
@@ -12,44 +16,196 @@ DEFAULT_PARAMS = {
 	"max_length": 30
 }
 
+def get_time() -> int:
+	"""
+	Get the current time, as formatted for createdAt and lastUpdatedAt
+
+	:return: Current time with millisecond precision
+	"""
+
+	return int(time() * 1000)
+
+def get_short_time() -> int:
+	"""
+	Because some lastUpdatedAt only are precise to the second
+
+	:return: Current time with second precision
+	"""
+
+	return int(time())
+
+# FIXME: store the constants, like versions, in a global constants file
+def create_new_stories_data(meta: str, current_time: int, current_time_short: int) -> Dict[str, Any]:
+	return {
+		"id": None,
+		"type": "stories",
+		"meta": meta,
+		"data": {
+			"storyMetadatVersion": 1,
+			#what to do with id, remoteId, remoteStoryId ?
+			"title": "New story",
+			"description": "",
+			"textPreview": "",
+			"favorite": False,
+			"tags": [],
+			"createdAt": current_time,
+			"lastUpdatedAt": current_time,
+			"isModified": False
+		},
+		"lastUpdatedAt": current_time_short,
+		"changedIndex": 0,
+		"nonce": random(SecretBox.NONCE_SIZE),
+		"decrypted": True,
+		"compressed": False
+	}
+
+def create_new_storycontent_data(meta: str, current_time_short: int) -> Dict[str, Any]:
+	return {
+		"id": None,
+		"storycontent": "storycontent",
+		"meta": meta,
+		"data": {
+			"storyContentVersion": 3,
+			"settings": {
+				"parameters": {},			# FIXME: default preset ?
+				"preset": "",
+				"trimResponse": True,
+				"banBrackets": True,
+				"prefix": ""
+			},
+			# FIXME: refactor this into a function
+			"story": {
+				"version": 2,
+				"step": 0,
+				"datablocks": [ {
+					"nextBlock": [],
+					"prevBlock": -1,
+					"origin": "root",
+					"startIndex": 0,
+					"endIndex": 0,
+					"dataFragment": { "data": "", "origin": "root" },
+					"fragmentIndex": -1,
+					"removedFragments": [],
+					"chain": False
+				} ],
+				"currentBlock": 0,
+				"fragments": [ { "data": "", "origin": "root" } ]
+			},
+			"context": [
+				{
+					"text": "",
+					"contextConfig": {},		# FIXME: fill me
+				},
+				{
+					"text": "",
+					"contextConfig": {},		# FIXME: fill me
+				}
+			],
+			"lorebook": {
+				"lorebookVersion": 3,
+				"entries": [],
+				"settings": { "orderByKeyLocations": False },
+				"categories": []
+			},
+			"storyContextConfig": {},			# FIXME: fill me
+			"ephemeralContext": [],
+			"contextDefaults": {
+				"ephemeralDefaults": [ {} ],	# FIXME: fill me
+				"loreDefaults": [ {} ],			# FIXME: fill me
+			},
+			"settingsDirty": False
+		},
+		"lastUpdatedAt": current_time_short,
+		"changeIndex": 0,
+		"decrypted": True,
+		"compressed": True
+	}
+
 class NovelAI_StoryProxy:
 	_parent: "NovelAI_Story"
 
 	_api: NovelAI_API
-	_keystore: Dict[str, bytes]
 	_key: bytes
 	_story: Dict[str, Any]
+	_storycontent: Dict[str, Any]
 	model: str
-	params: Dict[str, Any]
 
-	def __init__(self, parent: "NovelAI_Story", api: NovelAI_API, key: bytes, story: Dict[str, Any], model: Optional[str] = None, params: Optional[Dict[str, Any]] = None):
+	def __init__(self, parent: "NovelAI_Story", key: bytes, story: Dict[str, Any], storycontent: Dict[str, Any], model: Optional[str] = None):
 		self._parent = parent
 
-		self._api = api
+		self._api = parent._api
 		self._key = key
 		self._story = story
+		self._storycontent = storycontent
 
 		self._model = DEFAULT_MODEL if model is None else model
-		self._params = DEFAULT_PARAMS.copy() if params is None else params
 
 	async def generate(self, input: Union[str, List[int]]) -> "NovelAI_StoryProxy":
 		output = self._api.low_level.generate(input, self.model, self.params)
-		# TODO
+
+		story = self._storycontent["data"]["story"]
+		blocks = story["datablocks"]
+		fragments = story["fragments"]
+
+		cur_index = story["currentBlock"]
+		cur_block = blocks[cur_index]
+
+		story["step"] += 1
+
+		fragment = { "data": output, "origin": "" }
+		frag_index = len(fragments)
+		fragments.append(fragment)
+
+		start = cur_block["endIndex"] + 1
+		end = start + len(output)
+
+		block = {
+			"nextBlock": [],
+			"prevBlock": cur_index,
+			"origin": "",
+			"startIndex": start,
+			"endIndex": end,
+			"dataFragment": fragment,
+			"fragmentIndex": frag_index,
+			"removedFragments": [],
+			"chain": False
+		}
+		new_index = len(blocks)
+		blocks.append(block)
+
+		story["currentBlock"] = new_index
 
 	async def undo(self):
-		pass
+		story = self._storycontent["data"]["story"]
+
+		cur_index = story["currentBlock"]
+		blocks = story["datablocks"]
+	
+		cur_block = blocks[cur_index]
+		story["currentBlock"] = cur_block["prevBlock"]
 
 	async def save(self, upload: bool = False):
-		pass
+		encrypted_story = encrypt_user_data(self._story)
+		encrypted_storycontent = encrypt_user_data(self._storycontent)
+		# TODO
 
 	async def choose(self, index: int):
-		pass
+		story = self._storycontent["data"]["story"]
+
+		cur_index = story["currentBlock"]
+		blocks = story["datablocks"]
+	
+		cur_block = blocks[cur_index]
+		next_blocks = cur_block["nextBlock"]
+		assert 0 <= index < len(next_blocks), f"Expected index between 0 and {len(next_blocks)}, but got {index}"
+
+		story["currentBlock"] = next_blocks[index]
 
 	async def flatten(self):
 		pass
 
 	async def delete(self):
-		return self._parent.delete(self._story)
+		pass
 
 	async def get_current_tree(self):
 		pass
@@ -61,41 +217,49 @@ class NovelAI_Story:
 	_keystore: Dict[str, bytes]
 	_stories: Dict[str, Dict[str, Any]]
 
-	def __init__(self, api: NovelAI_API, keystore: Dict[str, bytes], stories: Dict[str, Dict[str, Any]]):
+	def __init__(self, api: NovelAI_API, keystore: Dict[str, bytes]):
 		self._api = api
 		self._keystore = keystore
-		self._stories = stories
 
-	def create(self):
+	def create(self) -> NovelAI_StoryProxy:
+		# FIXME: make a keystore central storage (create, delete, get) to take care of the garbage collection
 		meta = str(uuid4())
 		while meta in self._keystore:
 			meta = str(uuid4())
 
-		key = urandom(SecretBox.KEY_SIZE)
+		key = random(SecretBox.KEY_SIZE)
 		self._keystore[meta] = key
 
-		story = {
-			"id": None,
-			"meta": meta,
-			"data": "",
-			"content": [],
-			"decrypted": True
-		}
+		current_time = get_time()
+		current_time_short = get_short_time()
 
-		proxy = NovelAI_StoryProxy(self, self._api, key, story)
+		story = create_new_stories_data(meta, current_time, current_time_short)
+		storycontent = create_new_storycontent_data(meta, current_time_short)
+
+		proxy = NovelAI_StoryProxy(self, key, story, storycontent)
 		self._story_instances.append(proxy)
+
 		return proxy
 
-	def select(self, id: str):
-		story = {}	# FIXME
+	def load(self, story, storycontent) -> NovelAI_StoryProxy:
+		"""
+		Load a story proxy from a story and storycontent object
+		"""
+		assert story["meta"] == storycontent["meta"], f"Expected meta {story['meta']} for storycontent, but got meta {storycontent['meta']}"
 
-		meta = story["meta"]
-		assert meta in self._keystore, "Selected story has no encryption key"
-		key = self._keystore[meta]
+		proxy = NovelAI_StoryProxy(self, self._keystore[story["meta"]], story, storycontent)
+		# FIXME: look for duplicates
+		self._story_instances.append(proxy)
 
-		proxy = NovelAI_StoryProxy(self, self._api, key, story)
-		self._story_instances.append(proxy)		
-		return proxy
 
-	def delete(self, story: Dict[str, Any]):
-		pass
+	def select(self, id: str) -> NovelAI_StoryProxy:
+		"""
+		Select a story proxy from the previously created/loaded ones
+		"""
+
+		# TODO
+
+	def unload(self, id: str):
+		"""
+		Unload a previously created/loaded story, free'ing the NovelAI_StoryProxy object
+		"""
