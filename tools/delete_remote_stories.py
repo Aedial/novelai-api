1f5c22bf66c6590e5a76544b9d701ba9eca091b3
Aedial <aedial.dev@gmail.com>
2022-03-27 01:44:37 +0100

    [TOOL] Minor fixes and QoL additions
    Debugging is finished, should be bug-free

--- tools/delete_remote_stories.py
+++ tools/delete_remote_stories.py
@@ -2,7 +2,7 @@ from sys import path
 from os import environ as env
 from os.path import join, abspath, dirname
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentError
 
 path.insert(0, abspath(join(dirname(__file__), '..')))
 
@@ -15,7 +15,7 @@ from asyncio import run
 from json import dumps
 from datetime import datetime
 
-from typing import Dict, Any, NoReturn
+from typing import Dict, Any, NoReturn, List, Union
 
 if "NAI_USERNAME" not in env or "NAI_PASSWORD" not in env:
     raise RuntimeError("Please ensure that NAI_USERNAME and NAI_PASSWORD are set in your environment")
@@ -25,6 +25,16 @@ password = env["NAI_PASSWORD"]
 
 logger = Logger("NovelAI")
 logger.addHandler(StreamHandler())
+
+def str_to_bool(s: str) -> bool:
+    ret = True if s.lower() == "true" else \
+          False if s.lower() == "false" else \
+          None
+
+    if ret is None:
+        raise ArgumentError()
+
+    return ret
 
 def save_story(i: int, story: Dict[str, Any], args: ArgumentParser) -> NoReturn:
     name = story["metadata"]["title"]
@@ -59,23 +69,27 @@ def shelves_predicate(args: ArgumentParser, story: Dict[str, Any]) -> bool:
     return (args.exclude_nonshelf ^ (shelf is None)) and \
            (shelf is None or shelf in args.included_shelves)
 
+def match_and_then_or(args: List[List[str]], to_match: Union[str, List[str]]) -> bool:
+    return any(all((e in to_match) for e in items) for items in args)
+
 def memory_predicate(args: ArgumentParser, story: Dict[str, Any]) -> bool:
     memory = story["content"]["context"][0]["text"]
 
     return (args.has_memory is None or args.has_memory is bool(memory)) and \
-           (len(args.string_in_memory) == 0 or any(s in memory for s in args.string_in_memory))
+           (len(args.string_in_memory) == 0 or match_and_then_or(args.string_in_memory, memory))
 
 def an_predicate(args: ArgumentParser, story: Dict[str, Any]) -> bool:
     an = story["content"]["context"][1]["text"]
 
     return (args.has_an is None or args.has_an is bool(an)) and \
-           (len(args.string_in_an) == 0 or any(s in memory for s in args.string_in_an))
+           (len(args.string_in_an) == 0 or match_and_then_or(args.string_in_an, an))
 
 def lorebook_predicate(args: ArgumentParser, story: Dict[str, Any]) -> bool:
-    lorebook = story["content"]["lorebook"]["entries"]
+    entries = story["content"]["lorebook"]["entries"]
+    keys = [key for e in entries for key in e["keys"]]
 
-    return (args.has_lorebook is None or args.has_lorebook is bool(lorebook)) and \
-           (len(args.keys_in_lorebook) == 0 or any(key in args.keys_in_lorebook for e in lorebook for key in e["keys"]))
+    return (args.has_lorebook is None or args.has_lorebook is bool(keys)) and \
+           (len(args.key_in_lorebook) == 0 or match_and_then_or(args.key_in_lorebook, keys))
 
 PREDICATES = (
     creation_date_predicate,
@@ -87,28 +101,29 @@ PREDICATES = (
 )
 
 async def main():
-    parser = ArgumentParser()
+    parser = ArgumentParser(description = "Delete the stories matching all the selected filters, and save them in <backup_directory>. Filters that are not set are ignored")
     parser.add_argument("backup_directory", nargs = "?", default = ".", help = "Directory to save the deleted stories to")
 
-    parser.add_argument("--created-before", type = int, required = False)
-    parser.add_argument("--created-after", type = int, required = False)
-    parser.add_argument("--created-between", type = int, required = False, nargs = 2, default = [], action = "append")
+    parser.add_argument("--created-before", type = int, required = False, help = "Select all items created strictly before this time (in ms since Epoch)")
+    parser.add_argument("--created-after", type = int, required = False, help = "Select all items created strictly after this time (in ms since Epoch)")
+    parser.add_argument("--created-between", type = int, required = False, nargs = 2, default = [], action = "append" , help = "Select all items created strictly between these times (in ms since Epoch)")
 
-    parser.add_argument("--last-updated-before", required = False)
-    parser.add_argument("--last-updated-after", required = False)
-    parser.add_argument("--last-updated-between", required = False, nargs = 2, default = [], action = "append")
+    parser.add_argument("--last-updated-before", type = int, required = False, help = "Select all items last updated strictly before this time (in ms since Epoch)")
+    parser.add_argument("--last-updated-after", type = int, required = False, help = "Select all items last updated strictly after this time (in ms since Epoch)")
+    parser.add_argument("--last-updated-between", type = int, required = False, nargs = 2, default = [], action = "append", help = "Select all items last updated strictly between these times (in ms since Epoch)")
 
+    # TODO: need to query shelves for that
 #    parser.add_argument("--exclude-nonshelf", required = False, action = "store_true")
-#    parser.add_argument("--included-shelves", type = str, required = False, default = [], nargs = '+')
+#    parser.add_argument("--included-shelves", type = str, required = False, default = [], nargs = '+', action = "extend")
 
-    parser.add_argument("--has-memory", type = bool, required = False)
-    parser.add_argument("--string-in-memory", type = str, required = False, default = [], nargs = '+')
+    parser.add_argument("--has-memory", type = str_to_bool, required = False, help = "Select all items having text in Memory or not")
+    parser.add_argument("--string-in-memory", type = str, required = False, default = [], nargs = '+', action = "append", help = "Select all items having the specified text in Memory. Text in the same argument are AND, text on a different argument are OR.")
 
-    parser.add_argument("--has-an", type = bool, required = False)
-    parser.add_argument("--string-in-an", type = str, required = False, default = [], nargs = '+')
+    parser.add_argument("--has-an", type = str_to_bool, required = False, help = "Select all items having text in A/N or not")
+    parser.add_argument("--string-in-an", type = str, required = False, default = [], nargs = '+', action = "append", help = "Select all items having the specified text in A/N. Text in the same argument are AND, text on a different argument are OR.")
 
-    parser.add_argument("--has-lorebook", type = bool, required = False)
-    parser.add_argument("--keys-in-lorebook", type = str, required = False, default = [], nargs = '+')
+    parser.add_argument("--has-lorebook", type = str_to_bool, required = False, help = "Select all items having a Lorebook or not")
+    parser.add_argument("--key-in-lorebook", type = str, required = False, default = [], nargs = '+', action = "append", help = "Select all items having the specified keys in Lorebook. Keys in the same argument are AND, keys on a different argument are OR.")
 
     args = parser.parse_args()
     print(vars(args))
@@ -140,9 +155,9 @@ async def main():
                     save_story(i, content, args)
 
                     story_id = story["id"]
-#                    api.low_level.delete_object("stories", story_id)
+                    await api.low_level.delete_object("stories", story_id)
 
                     storycontent_id = story["content"]["id"]
-#                    api.low_level.delete_object("storycontent", storycontent_id)
+                    await api.low_level.delete_object("storycontent", storycontent_id)
 
 run(main())
