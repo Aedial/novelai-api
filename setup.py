6c4c011bc93cf465dceb20be2fa1058d2caa672a
Aedial <aedial.dev@gmail.com>
2022-03-30 07:18:47 +0200

    [MISC] Bump version to 0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "novelai-api",
-    version = "0.8",
+    version = "0.9",
     author = "Arthus Leroy",
     author_email = "arthus.leroy@epita.fr",
     url = "https://github.com/arthus-leroy/novelai-api/",
@@ -33,6 +33,6 @@ setuptools.setup(
 		"argon2-cffi",
 		"pynacl",
         "jsonschema",
-        "transformers"
+        "tokenizers",
 	]
 )
