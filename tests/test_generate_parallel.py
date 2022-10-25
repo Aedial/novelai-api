e31c1f8fa2ae418a463134d7abcd10a3dd64636e
Aedial <aedial.dev@gmail.com>
2022-10-25 05:26:47 +0200

    [TEST] Shorten test parallel to (hopefully) reduce the number of failures

--- tests/test_generate_parallel.py
+++ tests/test_generate_parallel.py
@@ -30,7 +30,7 @@ logger = Logger("NovelAI")
 logger.addHandler(StreamHandler())
 
 
-async def generate_10(api: NovelAIAPI, model: Model):
+async def generate_5(api: NovelAIAPI, model: Model):
     await api.high_level.login(username, password)
 
     preset = Preset.from_default(model)
@@ -41,8 +41,8 @@ async def generate_10(api: NovelAIAPI, model: Model):
     input_txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at dolor dictum, interdum est sed, consequat arcu. Pellentesque in massa eget lorem fermentum placerat in pellentesque purus. Suspendisse potenti. Integer interdum, felis quis porttitor volutpat, est mi rutrum massa, venenatis viverra neque lectus semper metus. Pellentesque in neque arcu. Ut at arcu blandit purus aliquet finibus. Suspendisse laoreet risus a gravida semper. Aenean scelerisque et sem vitae feugiat. Quisque et interdum diam, eu vehicula felis. Ut tempus quam eros, et sollicitudin ligula auctor at. Integer at tempus dui, quis pharetra purus. Duis venenatis tincidunt tellus nec efficitur. Nam at malesuada ligula."
     prompt = Tokenizer.encode(model, input_txt)
 
-    preset["max_length"] = 100
-    gens = [api.high_level.generate(prompt, model, preset, global_settings) for _ in range(10)]
+    preset["max_length"] = 20
+    gens = [api.high_level.generate(prompt, model, preset, global_settings) for _ in range(5)]
     results = await gather(*gens)
     for i, gen in enumerate(results):
         logger.info(f"Gen {i}:")
@@ -51,26 +51,26 @@ async def generate_10(api: NovelAIAPI, model: Model):
 
 
 @pytest.mark.parametrize("model", models)
-async def test_run_10_generate_sync(model: Model):
+async def test_run_5_generate_sync(model: Model):
     # sync handler
     api = NovelAIAPI()
-    await generate_10(api, model)
+    await generate_5(api, model)
 
 
 @pytest.mark.parametrize("model", models)
-async def test_run_10_generate_async(model: Model):
+async def test_run_5_generate_async(model: Model):
     # async handler
     try:
         async with ClientSession() as session:
             api = NovelAIAPI(session)
-            await generate_10(api, model)
+            await generate_5(api, model)
     except Exception as e:
         await session.close()
         raise e
 
 if __name__ == "__main__":
     async def main():
-        await test_run_10_generate_sync(Model.Sigurd)
-        await test_run_10_generate_async(Model.Sigurd)
+        await test_run_5_generate_sync(Model.Sigurd)
+        await test_run_5_generate_async(Model.Sigurd)
 
     run(main())
