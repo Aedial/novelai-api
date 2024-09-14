36dfbc7a38f755282b87dce56466055e6bbf32bf
Aedial <Aedial@users.noreply.github.com>
2024-09-14 14:44:23 +0000

    deploy: 1a783c4a94a3eed9e80a5ec149020a563fb95b81

--- _static/js/hoverxref.js
+++ _static/js/hoverxref.js
@@ -111,7 +111,7 @@ function addTooltip(target) {
                 var url = getEmbedURL(href);
                 $.ajax({
                     url: url,
-                    headers: {'X-HoverXRef-Version': '1.4.0'},
+                    headers: {'X-HoverXRef-Version': '1.4.1'},
                 }).done(
                     function (data) {
                         // call the 'content' method to update the content of our tooltip with the returned data.
@@ -202,7 +202,7 @@ $(document).ready(function() {
         var url = getEmbedURL(href);
         $.ajax({
             url: url,
-            headers: {'X-HoverXRef-Version': '1.4.0'},
+            headers: {'X-HoverXRef-Version': '1.4.1'},
         }).done(
             function (data) {
                 var content = $('<div></div>');
